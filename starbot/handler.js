e0d061d7df635f52d9e8a9e786ab391385cfcf33
richardhill <richardmatthewhill@hotmail.com>
2021-05-04 22:07:04 +0100

    Updated code

--- starbot/handler.js
+++ starbot/handler.js
@@ -1,12 +1,22 @@
-'use strict'
+"use strict";
+
+const axios = require("axios");
 
 module.exports = async (event, context) => {
-  const result = {
-    'body': JSON.stringify(event.body),
-    'content-type': event.headers["content-type"]
+  console.log(process.env);
+
+  let res = await axios.get(process.env.query_url);
+  let body = `There are currently ${res.data.number} astronauts in space Bruno and Ricardo`;
+
+  if (process.env.add_people) {
+    body += " Including ";
+    res.data.people.forEach((p) => {
+      body += " " + p.name;
+    });
   }
 
   return context
     .status(200)
-    .succeed(result)
-}
+    .headers({ "Content-type": "application/json" })
+    .succeed(body);
+};
